95e1bd49f7ef0460ed80de6205aab06c29958d1b
krishnarjunsingh <krishnarjun.singh2008@gmail.com>
2025-07-26 17:31:50 +0530

    Community changes

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css';
-
+import MainHeader from '@/components/main-header'
 export const metadata = {
   title: 'NextLevel Food',
   description: 'Delicious meals, shared by a food-loving community.',
@@ -30,6 +30,7 @@ export default function RootLayout({ children }) {
           </svg>
         </div>
 
+        <MainHeader />
         {children}
       </body>
     </html>
